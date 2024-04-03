e5e39df33347b4912f8cf9a67c7c05807a13b191
yepvaishz <poti.vaishnavi21@gmail.com>
2024-04-03 13:55:48 +0530

    minor additions

--- HandWave/HandWave/urls.py
+++ HandWave/HandWave/urls.py
@@ -4,5 +4,6 @@ from django.http import HttpResponse
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('base.urls')),
+    #path('', include('base.urls')),
+    path('', include('detectApp.urls'))
 ]
